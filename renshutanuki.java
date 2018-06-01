e16fce3f6fb581ab5870880268ba0ea15b8c42e0
ShoutaKatayama114514 <shota_katayama@unirita.co.jp>
2018-06-01 15:48:47 +0900

    練習問題1

---
+++ renshutanuki.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class renshutanuki {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		String input = new Scanner(System.in).next();
+		input = input.replace("ta", "");
+		System.out.println(input);
+		
+	}
+
+}
