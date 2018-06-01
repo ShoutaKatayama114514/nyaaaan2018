17bd3a3b2fb4552a5aa2974eb2210e461bb80675
ShoutaKatayama114514 <shota_katayama@unirita.co.jp>
2018-06-01 16:31:51 +0900

    練習問題2

---
+++ PPAP.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PPAP {
+
+	//@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		String input = new Scanner(System.in).nextLine();
+		
+		char[] into = input.toCharArray();
+		
+		input = input.replace("ta", "");
+		
+	}
+
+}
